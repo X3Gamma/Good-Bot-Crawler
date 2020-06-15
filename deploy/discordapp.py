1e2720e392dd46d997bc4641e6ddba407782c76d
LAPTOP GIT <29891969+X3Gamma@users.noreply.github.com>
2020-06-15 14:55:22 +0800

    Development structure prepared

---
+++ deploy/discordapp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# pragma pylint: disable=unused-argument, no-self-use
+# License/Copyright <<INCLUDE LICENSE>>
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
